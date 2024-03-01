107eaf7ef8c264834b83c1fa255a36481bf14083
Lakshay Jain <lakshaybusiness.12@gmail.com>
2024-03-01 23:50:48 +0530

    creating userid and storing in socket

--- index.js
+++ index.js
@@ -4,16 +4,23 @@ const {createServer} = require("http")
 const {Server} = require("socket.io")
 const {join}=require("node:path")
 
+
 const app = express()
 const server=  createServer(app)
 const io = new Server(server)
- 
-app.use(express.static('public'))
+app.use(express.static(join(__dirname, 'public')));
+
 app.get("/",(req,res)=>{
   res.sendFile(join(__dirname,"index.html"))
 })
 io.on("connect",(socket)=>{
     console.log("user connected")
+
+    socket.on("endpoint",(msg)=>{
+          console.log(msg.receiver)
+         socket.endpoint = msg.receiver
+         console.log("Updated",socket.endpoint)
+    })
 })
 server.listen("4000")
 
