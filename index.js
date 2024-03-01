93141972ac649ff4a4bc2bc15406bc8fdf541ea0
Lakshay Jain <lakshaybusiness.12@gmail.com>
2024-03-02 01:31:14 +0530

    created room

--- index.js
+++ index.js
@@ -1,26 +1,27 @@
+const express = require("express");
+const { createServer } = require("http");
+const { Server } = require("socket.io");
+const { join } = require("node:path");
 
-const express = require("express")
-const {createServer} = require("http")
-const {Server} = require("socket.io")
-const {join}=require("node:path")
+const app = express();
+const server = createServer(app);
+const io = new Server(server);
+app.use(express.static(join(__dirname, "public")));
 
+app.get("/", (req, res) => {
+  res.sendFile(join(__dirname, "index.html"));
+});
+io.on("connect", (socket) => {
+  console.log("user connected");
 
-const app = express()
-const server=  createServer(app)
-const io = new Server(server)
-app.use(express.static(join(__dirname, 'public')));
+  socket.on("endpoint", (msg) => {
+    socket.endpoint = msg.receiver;
+    console.log("Updated", socket.endpoint);
+  });
 
-app.get("/",(req,res)=>{
-  res.sendFile(join(__dirname,"index.html"))
-})
-io.on("connect",(socket)=>{
-    console.log("user connected")
-
-    socket.on("endpoint",(msg)=>{
-          console.log(msg.receiver)
-         socket.endpoint = msg.receiver
-         console.log("Updated",socket.endpoint)
-    })
-})
-server.listen("4000")
-
+  socket.on("joinroom", (msg) => {
+    socket.room = msg.room;
+    console.log("Updated", socket.room);
+  });
+});
+server.listen("4000");
